1a772f449eb21ab92373e5185b934eccbf1daef8
Michael Dayah <michael@dayah.com>
2019-12-07 02:38:46 -0500

    Fully functional.

--- merge-by-date.php
+++ merge-by-date.php
@@ -4,37 +4,41 @@ $inactive = [];
 $active["file"] = fopen($argv[1], "r");
 $inactive["file"] = fopen($argv[2], "r");
 
+find_next_start($active);
+find_next_start($inactive);
+
 while (!feof($active["file"]) || !feof($inactive["file"])) {
-	find_next_start($active);
 	if ($active["start"] > $inactive["start"])
 		swap($active, $inactive);
 	stream_until_close($active);
+	find_next_start($active);
+	echo "\n\n";
 }
 
 function stream_until_close(&$handle) {
 	$close = '/^Session Close \((.*)\): (.*)$/';
 	echo $handle["line"];
-	while ($line = remove_utf8_bom(fgets($handle["file"]))) {
+	while ($line = fgets($handle["file"])) {
+		$line = remove_utf8_bom($line);
 		preg_match($close, $line, $matches);
 		echo $line;
-		if ($matches) {
-			$handle["end"] = DateTime::createFromFormat('D M d H:i:s Y', trim($matches[2]));
+		if ($matches)
 			return TRUE;
-		}
 	}
 }
 
 function find_next_start(&$handle) {
 	$start = '/^Session Start \((.*)\): (.*)$/';
-	while ($line = remove_utf8_bom(fgets($handle["file"]))) {
+	while ($line = fgets($handle["file"])) {
+		$line = remove_utf8_bom($line);
 		preg_match($start, $line, $matches);
 		if ($matches) {
-			$handle["end"] = 0;
 			$handle["start"] = DateTime::createFromFormat('D M d H:i:s Y', trim($matches[2]));
 			$handle["line"] = $line;
 			return TRUE;
 		}
 	}
+	$handle["start"] = new DateTime();
 }
 
 function remove_utf8_bom($text) {
@@ -44,7 +48,6 @@ function remove_utf8_bom($text) {
 }
 
 function swap(&$x, &$y) {
-	echo "\n";
 	$tmp = $x;
 	$x = $y;
 	$y = $tmp;
